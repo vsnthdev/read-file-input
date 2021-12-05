4baa98990a32db6acc4c33b0221fcf52cb9bb014
Vasanth Developer <vsnthdev@gmail.com>
2021-12-06 00:25:53 +0530

    🔨 added JavaScript support for ESLint & Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,9 @@
  */
 
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['prettier', 'simple-import-sort', '@typescript-eslint', 'import'],
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
+    parser: '@babel/eslint-parser',
+    plugins: ['prettier', 'simple-import-sort', 'import'],
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
     env: {
         es2021: true,
         node: true,
@@ -19,18 +15,31 @@ module.exports = {
         ecmaVersion: 12,
         sourceType: 'module',
     },
-    settings: {
-        'import/extensions': ['.js'],
-    },
     rules: {
-        indent: ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['off', 'single'],
-        semi: ['error', 'never'],
         'prettier/prettier': 'error',
         'simple-import-sort/imports': 'error',
         'sort-imports': 'off',
         'import/order': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
     },
+    overrides: [
+        {
+            files: ['*.ts'],
+            parser: '@typescript-eslint/parser',
+            plugins: [
+                'prettier',
+                'simple-import-sort',
+                '@typescript-eslint',
+                'import',
+            ],
+            extends: [
+                'eslint:recommended',
+                'plugin:prettier/recommended',
+                'plugin:@typescript-eslint/recommended',
+            ],
+            rules: {
+                '@typescript-eslint/explicit-module-boundary-types': 'off',
+                '@typescript-eslint/no-explicit-any': 'off',
+            },
+        },
+    ],
 }
